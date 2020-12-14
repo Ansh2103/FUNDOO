24060813904a8e9ac6507efe56a3474ff11d6bde
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-12-14 11:29:09 +0530

    [SHUBHAM KUMAR] Added : Used decouplr config to prtect sensitive data and added .gitignore

--- fundoo/settings.py
+++ fundoo/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 import logging
 
+from decouple import config
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^*y5w-+1gm)=j8)0=jk_(1nrnjg0-ko_q9t(hj5gr^kz0!(u6%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -143,11 +145,11 @@ STATIC_URL = '/static/'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = "swayamshubham007.sm@gmail.com"
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # if not DEBUG:
 #     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
